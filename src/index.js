350e8c3f31ece9402dbe9348f15a82f9c09ffc52
Dayanalcazar88 <nanalcazar88@hotmail.com>
2024-05-16 20:40:40 -0500

    configuracion de express y puerto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("hola mundo");
+/* -------------------------- importar dependencias ------------------------- */
+import express from 'express'
+
+/* ----------------------------- initialization (framewor)----------------------------- */
+const app = express();
+
+/* -------------------------setting (configurar servidor y de el manejador de plantillas,asignando puerto)-------------------------------- */
+app.set('port',process.env.PORT || 3000)
+
+/* ------------------------------- middlewares(utilizar el morgan y ver las peticiones q llegan) ------------------------------ */
+
+
+
+
+/* --------------------------------- routes (crear rutas necesarias para el proyecto--------------------------------- */
+
+
+
+
+/* ------------------------------ public files(configiuracion para leer carpeta publica) ------------------------------ */
+
+
+
+
+/* ------------------------------- run server (encender el servidor)------------------------------- */
+app.listen(app.get('port'), ()=>{
+    console.log ('server listening on port', app.get('port'));
+
+});
