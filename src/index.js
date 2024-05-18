a406f1488451c68c2261ce61fc5576c70a5f5828
Dayanalcazar88 <nanalcazar88@hotmail.com>
2024-05-17 20:54:34 -0500

    Config Plantillas, rutas y peticiones

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
 /* -------------------------- importar dependencias ------------------------- */
 import express from 'express'
-
+import morgan from 'morgan'
+import {join,dirname, extname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
 /* ----------------------------- initialization (framewor)----------------------------- */
 const app = express();
+/* evita colisiones de nombre de variable */
+const __dirname = dirname(fileURLToPath(import.meta.url)); 
 
 /* -------------------------setting (configurar servidor y de el manejador de plantillas,asignando puerto)-------------------------------- */
 app.set('port',process.env.PORT || 3000)
+app.set('views', join(__dirname,'views'));
+app.engine('.hbs',engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts') ,
+    partialsDir:  join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set ('view engine','.hbs');
 
 /* ------------------------------- middlewares(utilizar el morgan y ver las peticiones q llegan) ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 
 
 
 
 /* --------------------------------- routes (crear rutas necesarias para el proyecto--------------------------------- */
+app.get('/', (req,res) => {
+    res.json({"message": "hola mundo"})
 
+});
 
 
 
 /* ------------------------------ public files(configiuracion para leer carpeta publica) ------------------------------ */
 
-
+app.use(express.static(join(__dirname,'public')))
 
 
 /* ------------------------------- run server (encender el servidor)------------------------------- */
