22b6a466b0b798d5658afbdde04d82ea7c56115a
Dayanalcazar88 <nanalcazar88@hotmail.com>
2024-05-22 18:53:23 -0500

    adicion boton delete

--- src/routers/personas.routes.js
+++ src/routers/personas.routes.js
@@ -3,12 +3,27 @@ import pool from '../database.js'
 
 const router = Router();
 
+
+/*----resultado consulta----*/
 router.get('/list', async(req, res) => {
     try {
         const [result] = await pool.query('SELECT * FROM persona');
         res.render('personas/list', {personas: result})
     } catch (error) {
         res.status(500).json({message: error.message});
+    }
+});
+
+/*elimina los datos n la base de datos desde boton eliminar en web*/
+
+router.get('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        await pool.query('DELETE FROM persona WHERE id = ?', [id]);
+        res.redirect('/list');
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
