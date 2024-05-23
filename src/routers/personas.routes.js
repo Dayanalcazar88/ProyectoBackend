1a5d6b46682e51f43312d1d7c8224e431eb6fb9b
Dayanalcazar88 <nanalcazar88@hotmail.com>
2024-05-22 20:43:06 -0500

    creamos a vistas add y la ruta y el post del add

--- src/routers/personas.routes.js
+++ src/routers/personas.routes.js
@@ -3,6 +3,27 @@ import pool from '../database.js'
 
 const router = Router();
 
+/*configuracion de add  agregar una persona*/
+router.get('/add', (req,res)=>{
+    res.render('personas/add')
+})
+
+
+router.post('/add', async (req,res) =>{
+
+    try {
+        const{name,last_name,age}= req.body
+        const newpersona = {name,last_name,age}
+
+        await pool.query('insert into persona set ?', [newpersona]);
+        res.redirect('/list');
+
+
+    }catch(error) {
+        res.status(500).json({message: error.message});
+    }
+
+    });
 
 /*----resultado consulta----*/
 router.get('/list', async(req, res) => {
@@ -28,3 +49,4 @@ router.get('/delete/:id', async (req, res) => {
 });
 
 export default router;
+
